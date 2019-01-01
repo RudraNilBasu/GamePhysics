b2bc608367b50a1abb119677b3bc27e4f504686a
Rudra Nil Basu <rudra.n.basu@gmail.com>
2019-01-01 19:39:12 +0530

    vector: Assign vector using vector
    No more breaking the vector into individual x and ys for assigning a
    vector
    
    Signed-off-by: Rudra Nil Basu <rudra.n.basu@gmail.com>

--- vectors.h
+++ vectors.h
@@ -21,6 +21,7 @@ typedef struct vec2 {
     vec2() : x(0.0f), y(0.0f) {}
 
     vec2(float _x, float _y) : x(_x), y(_y) {}
+    vec2(const vec2& v) : x(v.x), y(v.y) {}
 } vec2;
 
 typedef struct vec3 {
